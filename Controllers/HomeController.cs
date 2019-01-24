4df8dfaca56706ce66ef8a7c9614029b57b7e547
Brubaker <jbrubaker@curtisswright.com>
2019-01-24 14:35:04 -0500

    made editable

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,9 +33,23 @@ namespace nestedviewtest.Controllers
             var data = new List<ValvesViewModel>();
             ValvesViewModel vcvm = new ValvesViewModel();
             vcvm.CalculatedCost=100;
+            vcvm.EquipmentNumber="A";
             vcvm.ValveModel="26AD";
             data.Add(vcvm);
             return Json(data.ToDataSourceResult(request));
         }
+
+
+		[AcceptVerbs("Post")]
+		public ActionResult EditingInline_Update([DataSourceRequest] DataSourceRequest request, ValvesViewModel product)
+        {
+            if (product != null && ModelState.IsValid)
+            {
+                //request.Update(product);
+            }            
+
+            return Json(new[]{product}.ToDataSourceResult(request,ModelState));
+        }
+
     }
 }
